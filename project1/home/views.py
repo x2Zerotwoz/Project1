901d29366240daaffbca851303bf76afb94bee8d
x2Zerotwoz <papon1234xd@gmail.com>
2024-09-04 11:30:02 +0700

    first commit

--- project1/home/views.py
+++ project1/home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 def home(request):
-    return HttpResponse("Jujutsu Kaisen")
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
--- project1/members/views.py
+++ project1/members/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def members(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('members.html')
+    return HttpResponse(template.render())
 
 def profile(request):
-    return HttpResponse("Gojo Saroru")
+    template = loader.get_template('profile.html')
+    return HttpResponse(template.render())
